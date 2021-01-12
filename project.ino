5939cd28ab231766a00ad660145f4c0da931756d
w-mat <matwisniewski.1@gmail.com>
2021-01-12 21:51:32 +0100

    1.2_12.01.2021

--- project.ino
+++ project.ino
@@ -1,4 +1,16 @@
 //TO JEST TEST INO
-void setup() {
-    pinMode(13, OUTPUT);
-}
+int row_1 = 13; // Row 1 is Pin 13
+
+int row_2 = 12; // Row 2 is Pin 12
+
+int row_3 = 11; // Row 3 is Pin 11
+
+int row_4 = 10; // Row 4 is Pin 10
+
+int col_1 = 9; // Column 1 is Pin 9
+
+int col_2 = 8; // Column 2 is Pin 8
+
+int col_3 = 7; // Column 3 is Pin 7
+
+int col_4 = 6; // Column 4 is Pin 6
